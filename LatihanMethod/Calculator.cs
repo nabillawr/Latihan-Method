52c05518c81f59823bf392b3b7850c0cd9b2b692
nabillawr <nabillawr15@gmail.com>
2019-04-02 11:53:19 +0700

    LatihanMethod

---
+++ LatihanMethod/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    public class Calculator
+    {
+        public int Penjumlahan (int a, int b)
+        {
+            return a + b;
+        }
+
+        //overload method
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Pengurangan (int a, int b)
+        {
+            return a - b;
+        }
+
+        //method static
+        public static int Perkalian (int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
